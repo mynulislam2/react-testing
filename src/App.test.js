9734a96f279f1eb7f5941f100d530d90e11f4d36
mynul <homeexercise8@gmail.com>
2022-10-09 17:37:05 +0600

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+  render(<App />)
+  const colorbutton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorbutton).toHaveStyle({ backgroundColor: 'red' })
 });
+
