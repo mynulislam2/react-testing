15da45cfb967782730f7ae92f0f788319adb818e
mynul <homeexercise8@gmail.com>
2022-10-10 12:37:16 +0600

    second test

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { useState } from 'react';
 
 function App() {
 const [color,setColor] = useState("red")
+const [Check, setCheck] = useState(false)
 const newColor=color=="red"?"blue":"red"
 
   return (
     <div className="App">
-<button style={{backgroundColor:color}} onClick={()=>setColor(newColor)}>Change to {newColor}</button>
+<button style={{backgroundColor:color}} disabled={Check} onClick={()=>setColor(newColor)}>Change to {newColor}</button>
+<input type="checkbox" onClick={()=>setCheck(!Check)} name="" value=""/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,13 +4,30 @@ import App from './App';
 test('button has correct initial color', () => {
   render(<App />)
   const colorbutton = screen.getByRole('button', { name: 'Change to blue' });
-  
   expect(colorbutton).toHaveStyle({ backgroundColor: 'red' })
-
   fireEvent.click(colorbutton)
   expect(colorbutton).toHaveStyle({ backgroundColor: 'blue' })
   expect(colorbutton.textContent).toBe('Change to red')
-
-
 });
 
+
+test("initial conditions",()=>{
+  render(<App/>)
+const colorbutton=screen.getByRole("button",{name:"Change to blue"})
+expect(colorbutton).toBeEnabled()
+const checkbox=screen.getByRole("checkbox")
+expect(checkbox).not.toBeChecked()
+})
+
+
+test('checkbox disabled on fireEvent click and enables on second click', () => {
+  render(<App />)
+  const colorbutton=screen.getByRole('button')
+  const checkbox=screen.getByRole('checkbox')
+
+  fireEvent.click(checkbox)
+  expect(colorbutton).toBeDisabled()
+  fireEvent.click(checkbox)
+
+  expect(colorbutton).toBeEnabled()
+})
